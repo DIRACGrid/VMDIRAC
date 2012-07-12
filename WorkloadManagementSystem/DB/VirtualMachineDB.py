c9a33a38d935d92ba2b44ae5873028238bb7793f
Víctor Méndez <vmendez@pic.es>
2012-07-12 17:24:31 +0200

    typos

--- WorkloadManagementSystem/DB/VirtualMachineDB.py
+++ WorkloadManagementSystem/DB/VirtualMachineDB.py
@@ -404,7 +404,7 @@ class VirtualMachineDB( DB ):
 
     fields = ['Name', 'Endpoint','VMImageID', 'Status', 'LastUpdate' ]
     values = [instanceName, endpoint, imageID, validStates[0], DIRAC.Time.toString() ]
-    result = self.__getRunningPodDict( runningPodName )
+    result = self.getRunningPodDict( runningPodName )
     if not result[ 'OK' ]:
       return result
     runningPodDict = result[ 'Value' ]
@@ -600,7 +600,7 @@ class VirtualMachineDB( DB ):
       # The image exits in DB, has to match
       imageID = imageID['Value'][0][0]
 
-    result = self.__getRunningPodDict( runningPodName )
+    result = self.getRunningPodDict( runningPodName )
     if not result['OK']:
       return result
     runningPodDict = result[ 'Value' ]
@@ -987,13 +987,6 @@ class VirtualMachineDB( DB ):
     return self._query( sqlQuery )
 
   def getRunningPodDict( self, runningPodName ):
-    """
-    Wraper to private function
-    """
-    return self.__getRunningPodDict ( runningPodName )
-   
-
-  def __getRunningPodDict( runningPodName ):
     """
     Return from CS a Dictionary with RunningPod definition
     """

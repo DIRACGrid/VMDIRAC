2f14545daac7440b22f0d3cf3739459a98441c03
ubeda <mario.ubeda.garcia@cern.ch>
2013-03-01 17:17:17 +0100

    FIX: code beautification ( pylint warnings removal )

--- WorkloadManagementSystem/private/AmazonDirector.py
+++ WorkloadManagementSystem/private/AmazonDirector.py
@@ -3,13 +3,18 @@
 # File :   AmazonDirector.py
 # Author : Ricardo Graciani
 ########################################################################
-__RCSID__ = "$Id: AmazonDirector.py 16 2010-03-15 11:39:29Z ricardo.graciani@gmail.com $"
 
-from DIRAC import S_OK, S_ERROR
+#DIRAC
+from DIRAC import S_OK
+
+#VMDIRAC
 from VMDIRAC.WorkloadManagementSystem.private.VMDirector import VMDirector
 from VMDIRAC.WorkloadManagementSystem.Client.AmazonImage import AmazonImage
 
+__RCSID__ = "$Id: AmazonDirector.py 16 2010-03-15 11:39:29Z ricardo.graciani@gmail.com $"
+
 class AmazonDirector( VMDirector ):
+  
   def __init__( self, submitPool ):
     self.Flavor = 'Amazon'
     VMDirector.__init__( self, submitPool )
@@ -28,12 +33,18 @@ class AmazonDirector( VMDirector ):
     """
     VMDirector.configureFromSection( self, mySection )
 
-  def _submitInstance( self, imageName, workDir ):
+  def _submitInstance( self, imageName, _workDir ):
     """
       Real backend method to submit a new Instance of a given Image
     """
-    ami = AmazonImage( imageName )
+    
+    ami    = AmazonImage( imageName )
     result = ami.startNewInstances()
+    
     if not result[ 'OK' ]:
       return result
+    
     return S_OK( result[ 'Value' ][0] )
+  
+#...............................................................................
+#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF

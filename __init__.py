b341e77ef42086bdb1e1f49cf8c5f83b1a72764b
ubeda <mario.ubeda.garcia@cern.ch>
2013-03-01 16:18:44 +0100

    FIX: added code beautification

--- __init__.py
+++ __init__.py
@@ -1,5 +1,18 @@
 # $HeadURL$
+''' VMDIRAC package
+  
+  DIRAC extension providing all necessary components to overcome the incompatibilities
+  of Grid software on a CloudComputing environment.
+  
+  DIRACWeb extension with controllers to enhance user experience providing a GUI
+  where to use the package.
+  
+  Two main components: Web ( DIRACWeb extension ) and WorkloadManagementSystem ( DIRAC ).
+       
+'''
+
 from pkgutil import extend_path
+#FIXME: why defined here ?
 __path__ = extend_path( __path__, __name__ )
 
 __RCSID__ = "$Id: __init__.py 46 2010-03-18 14:52:51Z adriancasajus@gmail.com $"
@@ -8,15 +21,19 @@ __RCSID__ = "$Id: __init__.py 46 2010-03-18 14:52:51Z adriancasajus@gmail.com $"
 
 majorVersion = 1
 minorVersion = 0
-patchLevel = 0
-preVersion = 1
+patchLevel   = 0
+preVersion   = 1
 
-version = "v%sr%s" % ( majorVersion, minorVersion )
+version      = "v%sr%s" % ( majorVersion, minorVersion )
 buildVersion = "v%dr%d" % ( majorVersion, minorVersion )
 
 if patchLevel:
-  version = "%sp%s" % ( version, patchLevel )
+  version      = "%sp%s"       % ( version, patchLevel )
   buildVersion = "%s build %s" % ( buildVersion, patchLevel )
+
 if preVersion:
-  version = "%s-pre%s" % ( version, preVersion )
+  version      = "%s-pre%s"  % ( version, preVersion )
   buildVersion = "%s pre %s" % ( buildVersion, preVersion )
+  
+################################################################################
+#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF

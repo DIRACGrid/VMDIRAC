ddbb4906cbb59f4ab9183f7165a9196f9bccad9e
ubeda <mario.ubeda.garcia@cern.ch>
2013-03-12 18:23:14 +0100

    FIX: Moved from WMS.private to Resources.Cloud

---
+++ Resources/Cloud/KVMDirector.py
@@ -0,0 +1,38 @@
+########################################################################
+# $HeadURL$
+# File :   KVMDirector.py
+# Author : Ricardo Graciani
+########################################################################
+
+# DIRAC
+from DIRAC import S_OK
+
+#VMDIRAC
+from VMDIRAC.Resources.Cloud.VMDirector import VMDirector
+
+__RCSID__ = "$Id: KVMDirector.py 16 2010-03-15 11:39:29Z ricardo.graciani@gmail.com $"
+
+class KVMDirector( VMDirector ):
+  def __init__( self, submitPool ):
+    self.Flavor = 'KVM'
+    VMDirector.__init__( self, submitPool )
+
+  def configure( self, csSection, submitPool ):
+    """
+     Here goes common configuration for KVM Director
+    """
+
+    VMDirector.configure( self, csSection, submitPool )
+    self.reloadConfiguration( csSection, submitPool )
+
+  def configureFromSection( self, mySection ):
+    """
+      reload from CS
+    """
+    VMDirector.configureFromSection( self, mySection )
+
+  def _submitInstance( self, imageName, workDir ):
+    """
+      Real backend method to submit a new Instance of a given Image
+    """
+    return S_OK()

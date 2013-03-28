79766d869b1539415954e87c447911a0343e808c
ubeda <mario.ubeda.garcia@cern.ch>
2013-03-28 13:37:28 +0100

    NEW: added FIXME

--- Resources/Cloud/CloudDirector.py
+++ Resources/Cloud/CloudDirector.py
@@ -77,6 +77,7 @@ class CloudDirector( VMDirector ):
       return S_OK( idInstance )
 
     if driver == 'nova-1.1':
+      #FIXME: getting instanceType here is a nonsense... we have it on the Context
       instanceType = gConfig.getValue( "/Resources/VirtualMachines/Images/%s/%s" % ( imageName, 'instanceType' ), "" )
       nima     = NovaImage( imageName, endpoint )
       connNova = nima.connectNova()

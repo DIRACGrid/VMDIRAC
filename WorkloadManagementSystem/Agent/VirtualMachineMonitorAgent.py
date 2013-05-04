6cc768b8d6c5fc11878372cc0ec82d0aca35cf25
Víctor Méndez <vmendez@pic.es>
2013-05-04 13:15:45 +0200

    no HEPiX sutdown or similar by the moment

--- WorkloadManagementSystem/Agent/VirtualMachineMonitorAgent.py
+++ WorkloadManagementSystem/Agent/VirtualMachineMonitorAgent.py
@@ -182,7 +182,6 @@ class VirtualMachineMonitorAgent( AgentModule ):
     result = self.__declareInstanceRunning()
     if not result[ 'OK' ]:
       self.log.error( "Could not declare instance running", result[ 'Message' ] )
-      self.shutdownMessage = '100 ' + 'VM could not be declared running. ' + result['Message']
       self.__haltInstance()
       return S_ERROR( "Halting!" )
     self.__instanceInfo = result[ 'Value' ]
@@ -279,7 +278,6 @@ class VirtualMachineMonitorAgent( AgentModule ):
       if self.vmStopPolicy == 'elastic':
         #If load less than X, then halt!
           if avgLoad < self.vmMinWorkingLoad:
-            self.shutdownMessage = '200 ' + 'VM without workload on the last window time. ' + str(self.haltBeforeMargin) + ' s.'
             self.__haltInstance( avgLoad )
       if self.vmStopPolicy == 'never':
         self.log.info( "VM stoppage policy is defined to never (until SaaS or site request)")
@@ -303,7 +301,6 @@ class VirtualMachineMonitorAgent( AgentModule ):
         except Exception, e:
           self.log.error( "Could not write stop agent file", stopFile )
     if 'halt' in hbMsg and hbMsg[ 'halt' ]:
-      self.shutdownMessage = '300' + 'VM hatl requested by: VMDIRAC'
       self.__haltInstance()
 
   def __haltInstance( self, avgLoad = 0 ):
@@ -319,21 +316,6 @@ class VirtualMachineMonitorAgent( AgentModule ):
       if i < retries - 1 :
         self.log.info( "Sleeping for %d seconds and retrying" % sleepTime )
         time.sleep( sleepTime )
-
-    if 'MACHINEFEATURES' in os.environ:
-      try:
-        s = open(os.environ['MACHINEFEATURES']+'/shutdown_message_broadcast', 'r')
-          shutdown_message_broadcast = s.read().strip()
-          s.close()
-           
-         try:
-           os.system(shutdown_message_broadcast + ' ' + self.shutdownMessage)
-         except AttributeError:
-           os.system(shutdown_message_broadcast)
-
-      except:
-        self.log.info( "shutdown_message_broadcast exception" )
-        pass
 
     self.log.info( "Executing system halt..." )
     os.system( "halt" )

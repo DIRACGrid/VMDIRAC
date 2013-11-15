0cf70279611fccaf7b163231118e3a697e8bcb23
Víctor Méndez <vmendez@pic.es>
2013-11-15 15:06:26 +0100

    debbuing

--- WorkloadManagementSystem/Agent/VirtualMachineMonitorAgent.py
+++ WorkloadManagementSystem/Agent/VirtualMachineMonitorAgent.py
@@ -315,7 +315,7 @@ class VirtualMachineMonitorAgent( AgentModule ):
           self.log.info( "Wrote stop file %s for agent %s" % ( stopFile, agentName ) )
         except Exception, e:
           self.log.error( "Could not write stop agent file", stopFile )
-    if 'halt' in hbMsg and hbMsg[ 'halt' ]:
+    if hbMsg == 'halt':
       self.__haltInstance()
 
   def __haltInstance( self, avgLoad = 0 ):

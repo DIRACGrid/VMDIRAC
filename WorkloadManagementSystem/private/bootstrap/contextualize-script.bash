3a9592254caf1995e928e70af546d4f48b64774b
Víctor Méndez <vmendez@pic.es>
2013-11-16 10:17:21 +0100

    debbuing

--- WorkloadManagementSystem/private/bootstrap/contextualize-script.bash
+++ WorkloadManagementSystem/private/bootstrap/contextualize-script.bash
@@ -120,10 +120,16 @@ fi
 
 #3) Run the dirac contextualization script:    
 
-echo "bash ${localDiracContextPath} ${siteName} ${vmStopPolicy} ${vmCertPath} ${vmKeyPath} ${localVmRunJobAgent} ${localVmRunVmMonitorAgent} ${localVmRunVmUpdaterAgent} ${localVmRunLogAgent} ${cloudDriver} ${submitPool} ${cpuTime}" >> /var/log/contextualize-script.log 2>&1
+if [ ${diracContextPath} != 'nouse' ]
+then
+    echo "Ready for running dirac contextualize script: ${localDiracContextPath}" >> /var/log/contextualize-script.log 2>&1
+    echo "    Parameters: ${siteName} ${vmStopPolicy} ${vmCertPath} ${vmKeyPath} ${localVmRunJobAgent} ${localVmRunVmMonitorAgent} ${localVmRunVmUpdaterAgent} ${localVmRunLogAgent} ${cloudDriver} ${submitPool} ${cpuTime}" >> /var/log/contextualize-script.log 2>&1
 
-chmod u+x ${localDiracContextPath} >> /var/log/contextualize-script.log 2>&1
-bash ${localDiracContextPath} "${siteName}" "${vmStopPolicy}" "${vmCertPath}" "${vmKeyPath}" "${localVmRunJobAgent}" "${localVmRunVmMonitorAgent}" "${localVmRunVmUpdaterAgent}" "${localVmRunLogAgent}" "${cloudDriver}" "${submitPool}" "${cpuTime}" 
+    chmod u+x ${localDiracContextPath} >> /var/log/contextualize-script.log 2>&1
+    bash ${localDiracContextPath} '${siteName}' '${vmStopPolicy}' '${vmCertPath}' '${vmKeyPath}' '${localVmRunJobAgent}' '${localVmRunVmMonitorAgent}' '${localVmRunVmUpdaterAgent}' '${localVmRunLogAgent}' '${cloudDriver}' '${submitPool}' '${cpuTime}' 
+else
+    echo "Context configured with 'nouse' of dirac contextualize script" >> /var/log/contextualize-script.log 2>&1
+fi
 
 echo "END /root/contextualize-script.bash" >> /var/log/contextualize-script.log
 

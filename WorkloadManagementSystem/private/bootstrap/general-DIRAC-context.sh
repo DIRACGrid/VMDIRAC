e18dbd29ab983ad8e1ee0cc4cb02a7bfbedfe0b2
Víctor Méndez <vmendez@pic.es>
2013-11-16 23:44:03 +0100

    one DIRAC-context scripts for each global defaults

--- WorkloadManagementSystem/private/bootstrap/general-DIRAC-context.sh
+++ WorkloadManagementSystem/private/bootstrap/general-DIRAC-context.sh
@@ -51,10 +51,14 @@ echo "11 $cloudDriver" >> /var/log/dirac-context-script.log 2>&1
 	su dirac -c'mkdir -p etc/grid-security' >> /var/log/dirac-context-script.log 2>&1
 	chmod -R 755 etc >> /var/log/dirac-context-script.log 2>&1
 	mv ${putCertPath} etc/grid-security/servercert.pem >> /var/log/dirac-context-script.log 2>&1
-	chmod 444 /root/servercert.pem >> /var/log/dirac-context-script.log 2>&1
 	mv ${putKeyPath} etc/grid-security/serverkey.pem >> /var/log/dirac-context-script.log 2>&1
-	chmod 400 /root/serverkey.pem >> /var/log/dirac-context-script.log 2>&1
-	chown dirac:dirac etc >> /var/log/dirac-context-script.log 2>&1
+
+	sleep 1
+
+	chmod 444 etc/grid-security/servercert.pem >> /var/log/dirac-context-script.log 2>&1
+	chmod 400 etc/grid-security/serverkey.pem >> /var/log/dirac-context-script.log 2>&1
+
+	chown -R dirac:dirac etc >> /var/log/dirac-context-script.log 2>&1
 	
 #
 # Installing DIRAC
@@ -89,7 +93,7 @@ echo "11 $cloudDriver" >> /var/log/dirac-context-script.log 2>&1
         # if CAs are not download we retry
         for retry in 0 1 2 3 4 5 6 7 8 9
         do
-		su dirac -c"dirac-configure -UHdd -o /LocalSite/SubmitPool=${submitPool} -o /LocalSite/CPUTime=${cpuTime} -o /LocalSite/CloudDriver=${cloudDriver} -o /LocalSite/Site=${siteName}  -o /LocalSite/VMStopPolicy=${vmStopPolicy}  -o /LocalSite/CE=CE-nouse defaults-VMDIRAC.cfg"  >> /var/log/dirac-context-script.log 2>&1
+		su dirac -c"dirac-configure -UHddd -o /LocalSite/SubmitPool=$submitPool -o /LocalSite/CPUTime=$cpuTime -o /LocalSite/CloudDriver=$cloudDriver -o /LocalSite/Site=$siteName  -o /LocalSite/VMStopPolicy=$vmStopPolicy  -o /LocalSite/CE=CE-nouse defaults-VMDIRAC.cfg"  >> /var/log/dirac-context-script.log 2>&1
 		# options H: SkipCAChecks, dd: debug level 2, U: UseServerCertificate 
 		# options only for debuging D: SkipCADownload
 		# after UseServerCertificate = yes for the configuration with CS

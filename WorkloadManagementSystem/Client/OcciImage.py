d411c386fcf1ef5ed4edcf3847fcc165a205a444
Víctor Méndez <vmendez.tic@gmail.com>
2014-01-11 20:14:58 +0100

    proxycacert in rocci now is autproxy

--- WorkloadManagementSystem/Client/OcciImage.py
+++ WorkloadManagementSystem/Client/OcciImage.py
@@ -69,10 +69,10 @@ class OcciImage:
     if auth == 'userpasswd':
       user = u
       secret = p
-    elif auth == 'proxycacert':
+    elif auth == 'proxy':
       userCredPath = u
     else:
-      self.__errorStatus = "auth not supported (userpasswd/proxycacert)" 
+      self.__errorStatus = "auth not supported (userpasswd/proxy)" 
       self.log.error( self.__errorStatus )
       return
 
@@ -94,7 +94,7 @@ class OcciImage:
         self.log.error( self.__errorStatus )
         return S_ERROR( self.__errorStatus )
     elif self.__occiConfig.cloudDriver() == 'rocci-1.1':
-      if auth == 'proxycacert':
+      if auth == 'proxy':
         from VMDIRAC.WorkloadManagementSystem.Client.Rocci11 import OcciClient
         self.__cliocci = OcciClient(userCredPath, self.__occiConfig.config(), self.__imageConfig.config())
       else:
--- WorkloadManagementSystem/Utilities/Configuration.py
+++ WorkloadManagementSystem/Utilities/Configuration.py
@@ -151,11 +151,11 @@ class OcciConfiguration( EndpointConfiguration ):
         return S_ERROR( 'user is None' )
       if self.__password is None:
         return S_ERROR( 'password is None' )
-    elif self.__auth == 'proxycacert':
+    elif self.__auth == 'proxy':
       if self.__userCredPath is None:
         return S_ERROR( 'userCredPath is None' )
     else:
-      return S_ERROR( 'endpoint auth: %s not defined (userpasswd/proxycacert)' % self.__auth)
+      return S_ERROR( 'endpoint auth: %s not defined (userpasswd/proxy)' % self.__auth)
     
     self.log.info( '*' * 50 )
     self.log.info( 'Displaying endpoint info' )
@@ -175,10 +175,10 @@ class OcciConfiguration( EndpointConfiguration ):
     
     if self.__auth == 'userpasswd':
       return ( self.__auth, self.__user, self.__password )
-    elif self.__auth == 'proxycacert':
+    elif self.__auth == 'proxy':
       return ( self.__auth, self.__userCredPath, 'Nouse' )
     else:
-      return S_ERROR( 'endpoint auth: %s not defined (userpasswd/proxycacert)' % self.__auth)
+      return S_ERROR( 'endpoint auth: %s not defined (userpasswd/proxy)' % self.__auth)
   
   def cloudDriver( self ):
     

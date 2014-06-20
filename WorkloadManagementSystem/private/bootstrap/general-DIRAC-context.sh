f52517bf956d02e4aaf82f36efefaeb91e1bf86d
Zhao Xianghu <zhaoxh@ihep.ac.cn>
2014-06-20 12:27:09 +0800

    recover to the last commit

--- WorkloadManagementSystem/private/bootstrap/general-DIRAC-context.sh
+++ WorkloadManagementSystem/private/bootstrap/general-DIRAC-context.sh
@@ -47,7 +47,6 @@ echo "11 $cloudDriver" >> /var/log/dirac-context-script.log 2>&1
 
 # servercert/serverkey previouslly to this script copied 
 #
-        chown dirac:dirac /opt/dirac
 	cd /opt/dirac
 	su dirac -c'mkdir -p etc/grid-security' >> /var/log/dirac-context-script.log 2>&1
 	chmod -R 755 etc >> /var/log/dirac-context-script.log 2>&1

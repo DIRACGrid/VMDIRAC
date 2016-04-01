3e9fbca38055598b9b656ace5c592acb8cf389a7
Víctor Méndez <vmendez.tic@gmail.com>
2016-04-02 00:29:58 +0200

    NEW: enmr cvmfs context setup file

--- WorkloadManagementSystem/private/bootstrap/cvmfs-enmr-context.sh
+++ WorkloadManagementSystem/private/bootstrap/cvmfs-enmr-context.sh
@@ -10,30 +10,27 @@ then
 	echo "cvmfs-enmr-context.sh <cvmfs_http_proxy>"
 fi	
 
-rpm --import https://cvmrepo.web.cern.ch/cvmrepo/yum/RPM-GPG-KEY-CernVM
-yum -y install https://ecsft.cern.ch/dist/cvmfs/cvmfs-release/cvmfs-release-latest.noarch.rpm
-yum -y update
-yum -y install cvmfs cvmfs-config-default
+rpm --import https://cvmrepo.web.cern.ch/cvmrepo/yum/RPM-GPG-KEY-CernVM >> /var/log/cvmfs-context-script.log 2>&1
+yum -y install https://ecsft.cern.ch/dist/cvmfs/cvmfs-release/cvmfs-release-latest.noarch.rpm >> /var/log/cvmfs-context-script.log 2>&1
+#yum -y update >> /var/log/cvmfs-context-script.log 2>&1
+yum -y install cvmfs cvmfs-config-default >> /var/log/cvmfs-context-script.log 2>&1
 
 
 
 	cat<<EOF>/etc/cvmfs/default.local
 CVMFS_SERVER_URL="http://cvmfs-egi.gridpp.rl.ac.uk:8000/cvmfs/@fqrn@;http://klei.nikhef.nl:8000/cvmfs/@fqrn@;http://cvmfsrepo.lcg.triumf.ca:8000/cvmfs/@fqrn@;http://cvmfsrep.grid.sinica.edu.tw:8000/cvmfs/@fqrn@"
 CVMFS_KEYS_DIR=/etc/cvmfs/keys/egi.eu
+#alternative setup
+#CVMFS_SERVER_URL="http://cvmfs-egi.gridpp.rl.ac.uk:8000/cvmfs/@org@.gridpp.ac.uk;http://cvmfs01.nikhef.nl/cvmfs/@org@.gridpp.ac.uk"
+#CVMFS_PUBLIC_KEY=/etc/cvmfs/keys/gridpp.ac.uk.pub
 CVMFS_CACHE_BASE=/home/cache
 CVMFS_REPOSITORIES=enmr.eu
 CVMFS_QUOTA_LIMIT=4000
 CVMFS_HTTP_PROXY="$1"
 EOF
 
-#alternative setup
-#CVMFS_SERVER_URL="http://cvmfs-egi.gridpp.rl.ac.uk:8000/cvmfs/@org@.gridpp.ac.uk;http://cvmfs01.nikhef.nl/cvmfs/@org@.gridpp.ac.uk"
-#CVMFS_PUBLIC_KEY=/etc/cvmfs/keys/gridpp.ac.uk.pub
 
-	cat<<EOF>/etc/fuse.conf
-user_allow_other
-EOF
-
+#alternative setup needs this:
 	cat<<EOF>/etc/cvmfs/keys/gridpp.ac.uk.pub
 -----BEGIN PUBLIC KEY-----
 MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAp7C4KDvOIEVJepuAHjxE
@@ -46,10 +43,15 @@ DwIDAQAB
 -----END PUBLIC KEY-----
 EOF
 
+	cat<<EOF>/etc/fuse.conf
+user_allow_other
+EOF
+
 # reaload configuration to activate new setup:
 cvmfs_config reload >> /var/log/cvmfs-context-script.log 2>&1
 cvmfs_config showconfig >> /var/log/cvmfs-context-script.log 2>&1
 service autofs start >> /var/log/cvmfs-context-script.log 2>&1
 chkconfig autofs on >> /var/log/cvmfs-context-script.log 2>&1
+cvmfs_config showconfig >> /var/log/cvmfs-context-script.log 2>&1
 
 exit 0

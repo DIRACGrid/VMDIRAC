2bdff8b3a6c92046373fb12751089616f42327ad
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2016-05-06 16:33:47 +0200

    VMDIRAC 2.0 structure

---
+++ VMDIRAC/Security/VmProperties.py
@@ -0,0 +1,10 @@
+# $HeadURL$
+__RCSID__ = "$Id$"
+#
+#
+VM_WEB_OPERATION = "VmWebOperation"
+#
+VM_RPC_OPERATION = "VmRpcOperation"
+
+#...............................................................................
+#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF

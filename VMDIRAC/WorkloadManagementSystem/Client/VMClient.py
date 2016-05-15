3992538c73e7e8154ced014d88f9655505ecd236
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2016-05-15 14:12:55 +0200

    New generic client

---
+++ VMDIRAC/WorkloadManagementSystem/Client/VMClient.py
@@ -0,0 +1,14 @@
+from DIRAC import gLogger
+from DIRAC.Core.Base.Client import Client
+
+class VMClient( Client ):
+
+  def __init__( self ):
+    """c'tor
+
+    :param self: self reference
+    """
+    Client.__init__( self )
+    self.log = gLogger.getSubLogger( "WorkloadManagement/VMClient" )
+    self.setServer( "WorkloadManagement/VirtualMachineManager" )
+

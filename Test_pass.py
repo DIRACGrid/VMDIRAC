7febe58bc109df24e9ccb55b755690999e5b2917
Rafal Grzymkowski <rafal.grzymkowski@ifj.edu.pl>
2017-05-31 10:20:10 +0200

    test example for vmdirac fixed

--- Test_pass.py
+++ Test_pass.py
@@ -8,8 +8,5 @@ class TestUM(unittest.TestCase):
     def test_numbers_3_4(self):
         self.assertEqual( 12, 12)
  
-    def test_strings_a_3(self):
-        self.assertEqual( 'dfsd', 'aaa')
- 
 if __name__ == '__main__':
     unittest.main()

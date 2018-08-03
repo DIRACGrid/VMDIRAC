eef0198a37ae61061eeaab0e3c1418f5e2fc7923
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2018-08-03 13:32:03 +0200

    Reworked to handle native interface

--- VMDIRAC/Resources/Cloud/KeystoneClient.py
+++ VMDIRAC/Resources/Cloud/KeystoneClient.py
@@ -6,70 +6,166 @@ __RCSID__ = '$Id$'
 import requests
 import json
 
-from DIRAC import S_OK, S_ERROR
+from DIRAC import S_OK, S_ERROR, gLogger
 
 class KeystoneClient():
   """
   """
-
-  def __init__(self, url, authArgs):
+  def __init__(self, url, parameters):
+    self.log = gLogger.getSubLogger("Keystone")
     self.url = url
-    self.authArgs = authArgs
+    self.apiVersion = 2
+    if "v3" in url:
+      self.apiVersion = 3
+    self.parameters = parameters
     self.token = None
+    self.project = self.parameters.get('Project')
+    self.projectID = ''
+    self.computeURL = None
+    self.imageURL = None
+    self.networkURL = None
+
+    #result = self.getToken()
 
   def getToken(self):
+
+    self.log.debug("Initializing for API version %d" % self.apiVersion)
+
+    if self.apiVersion == 2:
+      result = self.__getToken2()
+    else:
+      result = self.__getToken3()
+
+    return result
+
+  def __getToken2(self):
 
     if self.token is not None:
       return S_OK(self.token)
 
+    user = self.parameters.get('User')
+    password = self.parameters.get('Password')
+    if user and password:
+      authDict = {'auth': {"passwordCredentials": {"username": user,
+                                                   "password": password}
+                          }
+                 }
+      if self.project:
+        authDict['auth']['tenantName'] = self.project
+    elif self.parameters.get('Auth') == "voms":
+      authDict = {'auth': {'voms': True}}
+
+    authArgs = {}
+    caPath = self.parameters.get('CAPath')
+    if caPath:
+      authArgs['verify'] = caPath
+    if self.parameters.get('Proxy'):
+      authArgs['cert'] = self.parameters.get('Proxy')
+
+    authJson = json.dumps(authDict)
+
     try:
-      result = requests.post("%s/v2.0/tokens" % self.url,
-                             headers = {"Content-Type": "application/json"},
-                             **self.authArgs).json()
+      result = requests.post("%s/tokens" % self.url,
+                             headers = {"Content-Type": "application/json",
+                                        'Content-Length': str(len(json.dumps(authJson)))},
+                             data = authJson,
+                             **authArgs)
     except Exception as exc:
-      #print "AT >> getToken", exc
+      print "AT >> getToken", exc
       return S_ERROR('Failed to get keystone token: %s', str(exc))
 
-    #print "AT >>> getToken",result
+    output = result.json()
 
-    self.token = str(result['access']['token']['id'])
+    import pprint
+    pprint.pprint(output)
+
+    self.token = str(output['access']['token']['id'])
+    for endpoint in output['access']['serviceCatalog']:
+      if endpoint['type'] == 'compute':
+        self.computeURL = str(endpoint['endpoints'][0]['publicURL'])
+      elif endpoint['type'] == 'image':
+        self.imageURL = str(endpoint['endpoints'][0]['publicURL'])
+      elif endpoint['type'] == 'network':
+        self.networkURL = str(endpoint['endpoints'][0]['publicURL'])
     return S_OK(self.token)
 
-  def getTenants(self):
+  def __getToken3(self):
+
+    if self.token is not None:
+      return S_OK(self.token)
+
+    domain = self.parameters.get('Domain', "Default")
+    user = self.parameters.get('User')
+    password = self.parameters.get('Password')
+    authDict = {}
+    if user and password:
+      authDict = {'auth': {"identity": {"methods": ["password"],
+                                        "password": {"user": {"name": user,
+                                                              "domain": {"name": domain},
+                                                              "password": password
+                                                              }
+                                                    }
+                                        }
+                           }
+                  }
+
+    elif self.parameters.get('Auth') == "voms":
+      authDict['auth'] = {'voms': True}
+
+    if self.project:
+      authDict['auth']['scope'] = {"project": {"domain": {"name": domain},
+                                       "name": self.project
+                                       }
+                           }
+
+
+    authArgs = {}
+    caPath = self.parameters.get('CAPath')
+    if caPath:
+      authArgs['verify'] = caPath
+    if self.parameters.get('Proxy'):
+      authArgs['cert'] = self.parameters.get('Proxy')
+
+    authJson = json.dumps(authDict)
 
     try:
-      result = requests.get("%s/v2.0/tenants/" % self.url,
-                            headers = {"Content-Type": "application/json",
-                                       "X-Auth-Token": self.token},
-                            **self.authArgs).json()
+      result = requests.post("%s/auth/tokens" % self.url,
+                             headers = {"Content-Type": "application/json",
+                                        'Content-Length': str(len(json.dumps(authJson)))},
+                             data = authJson,
+                             **authArgs)
     except Exception as exc:
-      return S_ERROR( 'Failed to get tenants: %s' % str( exc ) )
+      print "AT >> getToken", exc
+      return S_ERROR('Failed to get keystone token: %s', str(exc))
 
-    tenants = [ tenant['name'] for tenant in result['tenants'] ]
-    return S_OK( tenants )
+    try:
+      self.token = result.headers['X-Subject-Token']
+    except Exception as exc:
+      return S_ERROR('Failed to get keystone token: %s', str(exc))
 
-  def getTenantToken(self, tenants):
+    output = result.json()
+    import pprint
+    pprint.pprint(output)
 
-    authArgs = dict(self.authArgs)
-    del authArgs['data']
+    if output['token']['project']['name'] == self.project:
+      self.projectID = output['token']['project']['id']
 
-    for tenant in tenants:
-      try:
-        data = '{"auth": {"voms": true, "tenantName": "%s"}}' % tenant
-        result = requests.post("%s/v2.0/tokens" % self.url,
-                               data = data,
-                               headers = {'Accept': 'application/json',
-                                          'X-Auth-Token': self.token,
-                                          'User-Agent': 'VMDIRAC v3r0' + ' ( OCCI/1.1 )',
-                                          'Content-Type': 'application/json',
-                                          'Content-Length': str(len(json.dumps(data)))},
-                                **authArgs).json()
-      except Exception as exc:
-        return S_ERROR('Can not get token for tenant', '%s: %s' % (tenant, repr(exc)))
 
-      if 'access' in result:
-        return S_OK( result['access']['token']['id'] )
+    for service in output['token']['catalog']:
+      if service['type'] == 'compute':
+        for endpoint in service['endpoints']:
+          if endpoint['interface'] == 'public':
+            self.computeURL = str(endpoint['url'])
 
-    return S_ERROR( 'Failed to get token for any tenant' )
+      elif service['type'] == 'image':
+        for endpoint in service['endpoints']:
+          if endpoint['interface'] == 'public':
+            self.imageURL = str(endpoint['url'])
 
+      elif service['type'] == 'network':
+        for endpoint in service['endpoints']:
+          if endpoint['interface'] == 'public':
+            self.networkURL = str(endpoint['url'])
+
+    return S_OK(self.token)
 

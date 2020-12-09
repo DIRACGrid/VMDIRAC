d34036b2a0f48b0282d6e8bb0c09d17f39c0988b
Simon Fayer <sf105@ic.ac.uk>
2020-12-09 13:53:12 +0000

    Make VirtualMachineDB test script python3 ready

--- tests/test_VirtualMachineDB.py
+++ tests/test_VirtualMachineDB.py
@@ -14,34 +14,34 @@ import DIRAC
 db = VirtualMachineDB()
 validStates = db.validInstanceStates
 
-print "checkImageStatus        ", db.checkImageStatus('Image3')
-# print db.checkImageStatus( 'name', 'flavor'*10, 'requirements' )
+print("checkImageStatus        ", db.checkImageStatus('Image3'))
+# print(db.checkImageStatus( 'name', 'flavor'*10, 'requirements' ))
 
 ret = db.insertInstance('Image3', 'instance')
-print "insertInstance          ", ret
+print("insertInstance          ", ret)
 ret = db.insertInstance('Image2', 'instance')
-print "insertInstance          ", ret
+print("insertInstance          ", ret)
 
 if not ret['OK']:
   DIRAC.exit()
 
-print type(ret['Value'])
-print "declareInstanceSubmitted", db.declareInstanceSubmitted(ret['Value'])
+print(type(ret['Value']))
+print("declareInstanceSubmitted", db.declareInstanceSubmitted(ret['Value']))
 id1 = DIRAC.Time.toString()
-print "declareInstanceRunning  ", db.declareInstanceRunning('Image3', id1, 'IP', 'ip')
+print("declareInstanceRunning  ", db.declareInstanceRunning('Image3', id1, 'IP', 'ip'))
 id2 = DIRAC.Time.toString()
-print "declareInstanceRunning  ", db.declareInstanceRunning('Image2', id2, 'IP', 'ip')
-print "declareInstanceRunning  ", db.instanceIDHeartBeat(id2, 1.0)
+print("declareInstanceRunning  ", db.declareInstanceRunning('Image2', id2, 'IP', 'ip'))
+print("declareInstanceRunning  ", db.instanceIDHeartBeat(id2, 1.0))
 
 for status in validStates:
-  print "get%10sInstances  " % status, db.getInstancesByStatus(status)
+  print("get%10sInstances  " % status, db.getInstancesByStatus(status))
 
-print "declareInstanceHalting  ", db.declareInstanceHalting(id1, 0.0)
-print "declareInstanceHalting  ", db.declareInstanceHalting(id2, 0.0)
+print("declareInstanceHalting  ", db.declareInstanceHalting(id1, 0.0))
+print("declareInstanceHalting  ", db.declareInstanceHalting(id2, 0.0))
 
-print "declareStalledInstances ", db.declareStalledInstances()
-print "declareStalledInstances ", db.declareStalledInstances()
+print("declareStalledInstances ", db.declareStalledInstances())
+print("declareStalledInstances ", db.declareStalledInstances())
 
 from DIRAC.WorkloadManagementSystem.DB.TaskQueueDB import TaskQueueDB
 tq = TaskQueueDB()
-print tq.retrieveTaskQueues()
+print(tq.retrieveTaskQueues())
